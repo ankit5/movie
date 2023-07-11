4a55d4ea758c5ea877a4fcca70cf448aa53098c3
ankit5 <vermankit5@gmail.com>
2023-07-11 10:26:21 +0530

    up23676443

--- themes/custom/psyplay/js/script.js
+++ themes/custom/psyplay/js/script.js
@@ -17,31 +17,13 @@ Drupal.behaviors.customConfig = {
     
   }
 
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-
-
-if( isMobile.any() ) {
-  alert('Mobile');
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+   alert('Mobile');
 }
+
+
+
 
 
 
