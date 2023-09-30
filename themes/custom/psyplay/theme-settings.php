e824ce40ad8da59a168f24c1d1f0b79f63f7b914
ankit5 <vermankit5@gmail.com>
2023-09-30 00:03:52 -0700

    update5

---
+++ themes/custom/psyplay/theme-settings.php
@@ -0,0 +1,47 @@
+<?php
+use Drupal\Core\Form\FormStateInterface;
+/**
+ * @file
+ * Custom setting for gypsum theme.
+ */
+function psyplay_form_system_theme_settings_alter(&$form, FormStateInterface &$form_state, $form_id = NULL) {
+
+ $form['movie'] = [
+    '#type'        => 'details',
+    '#title'       => t("Get Content Movie Replace Name"),
+  ];
+ $form['movie']['old_domain_name'] = [
+    '#type'          => 'textfield',
+    '#title'         => t('Old Domain name'),
+    '#description'   => t("Enter Old Domain name."),
+    '#default_value' => theme_get_setting('old_domain_name'),
+  ];
+
+  $form['movie']['new_domain_name'] = [
+    '#type'          => 'textfield',
+    '#title'         => t('New Domain name'),
+    '#description'   => t("Enter New Domain name."),
+    '#default_value' => theme_get_setting('new_domain_name'),
+  ];
+
+  $form['movie_iframe'] = [
+    '#type'        => 'details',
+    '#title'       => t("Iframe Movie Replace Name"),
+  ];
+
+  $form['movie_iframe']['iframe_old_domain_name'] = [
+    '#type'          => 'textfield',
+    '#title'         => t('Iframe Old Domain name'),
+    '#description'   => t("Enter Iframe Old Domain name."),
+    '#default_value' => theme_get_setting('iframe_old_domain_name'),
+  ];
+
+  $form['movie_iframe']['iframe_new_domain_name'] = [
+    '#type'          => 'textfield',
+    '#title'         => t('Iframe New Domain name'),
+    '#description'   => t("Enter Iframe New Domain name."),
+    '#default_value' => theme_get_setting('iframe_new_domain_name'),
+  ];
+
+
+}
