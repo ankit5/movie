44c528b268badbf5dd24a9eb13c5e287e693eece
ankit5 <vermankit5@gmail.com>
2023-10-07 12:30:15 +0530

    asd245

--- sites/techto.life/settings.php
+++ sites/techto.life/settings.php
@@ -668,7 +668,7 @@ $settings['update_free_access'] = FALSE;
  */
  $config['system.site']['name'] = 'Yomovies';
  $config['user.settings']['anonymous'] = 'Visitor';
- $config['system.theme']['default'] = 'psythemenew';
+ $config['system.theme']['default'] = 'bartik';
 
 /**
  * Load services definition file.
