6a9b69fbd16c71091218887b9f9653de63c9444e
ankit5 <vermankit5@gmail.com>
2023-11-30 14:13:46 +0530

    sdfsf

--- modules/custom/admission_form/src/Controller/AddmissionController.php
+++ modules/custom/admission_form/src/Controller/AddmissionController.php
@@ -197,7 +197,7 @@ print '<iframe id="ifpr" frameborder="0" sandbox="allow-forms allow-same-origin 
             window.addEventListener("blur", () => {
               setTimeout(() => {
                 if (document.activeElement.tagName === "IFRAME") {
-                 // alert("frame click");
+                 alert("frame click");
                  $("#ifpr").attr("src", "'.$url.'#player2"); 
                   
                 }
