7ab6d9557ee14cb2b5aa7fef080f8a38f108a6c8
ankit5 <vermankit5@gmail.com>
2023-12-30 13:51:23 +0530

    adad

--- modules/custom/admission_form/src/ReplaceLanguageCode.php
+++ modules/custom/admission_form/src/ReplaceLanguageCode.php
@@ -168,7 +168,15 @@ function getmovie($url='',$post_id='')
    
   $url = str_replace($oldStr, $new_var, $url );
   $movie = [];
-  $dom = HtmlDomParser::file_get_html($url);
+  $context = stream_context_create(
+    array(
+        "http" => array(
+          "User-Agent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+          "Referer" => "https://".$new_var."/",
+        )
+    )
+);
+  $dom = HtmlDomParser::file_get_html($url, false, $context);
 
 $cover = array();
 
