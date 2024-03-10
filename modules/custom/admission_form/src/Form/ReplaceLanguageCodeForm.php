64832b2114d2c2f8ba72b1a90e7d3c21064368b9
ankit5 <vermankit5@gmail.com>
2024-03-10 16:15:56 +0530

    asdff

--- modules/custom/admission_form/src/Form/ReplaceLanguageCodeForm.php
+++ modules/custom/admission_form/src/Form/ReplaceLanguageCodeForm.php
@@ -46,8 +46,8 @@ $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
    $query->condition('type', 'movie', '=');
   //  $query->condition('title', '%episode%', 'LIKE');
   //  $query->notExists('field_year');
-
-   $nids = $query->range(600,11000)->execute();
+  $query->orderBy('created', 'DESC');
+   $nids = $query->range(0,10000)->execute();
 
    $batch = [
      'title' => t('Replacing Language Code...'),
