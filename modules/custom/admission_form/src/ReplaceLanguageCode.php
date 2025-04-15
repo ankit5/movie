1dff4dbb299dbad864a93d0dc423c3d0d5a1c31f
ankit5 <vermankit5@gmail.com>
2025-04-15 14:46:59 +0530

    asdf

--- modules/custom/admission_form/src/ReplaceLanguageCode.php
+++ modules/custom/admission_form/src/ReplaceLanguageCode.php
@@ -120,7 +120,7 @@ foreach($node->get('field_tags')->getValue() as $key=>$value){
 if(array_key_exists('94',$field_tags)){ //punjabi
   if($_SERVER['SERVER_NAME']!='hdmovie20.lat'){
   //return new TrustedRedirectResponse('https://hdmovie20.lat'.$node->toUrl()->toString());
-  header('Location: https://hdmovie20.lat'.$node->toUrl()->toString());
+  header('Location: https://hdmovie2.gay'.$node->toUrl()->toString());
   print $node->toUrl()->toString();
   exit;
   throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
